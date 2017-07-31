159bd5c8c901c923ee6973bb8e2bb9e7e6fc8e07
Ed Sweezey <esweezey@yahoo.com>
2017-07-31 19:09:36 -0400

    react components in progress

---
+++ react-client/src/components/Register.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }  
+  render(){
+    return (
+      <div>
+        <title />
+      </div>
+    );
+  }
+}
+
+export default Register;
