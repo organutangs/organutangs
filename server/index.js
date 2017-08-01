5eecb28eb6f63ff7e81c755a7df60395f5ea5ea8
Michael Lam <mmmikeal@gmail.com>
2017-08-01 10:46:23 -0400

    testing

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(morgan('combined'));
 
 
 //this configures the LocalStrategy for username/password authentication
-//done is a callback(null, user)
+//done is a callback(null, user), exists req.user
 passport.use(new LocalStrategy(
   function(username, password, done) {
     User.findOne({ username: username }, function (err, user) {
@@ -44,28 +44,6 @@ passport.use(new LocalStrategy(
   }
 ));
 
-app.use(express.static(__dirname + '/../react-client/dist'));
-
-
-//michael new
-//
-
-
-//handles a login attempt from the front end
-//returns 401 unauthorized if fails
-app.post('/login',
-  passport.authenticate('local'),
-  function(req, res) {
-    // If this function gets called, authentication was successful.
-    // `req.user` contains the authenticated user.
-    res.redirect('/users/' + req.user.username);
-  });
-
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
-);
 
 //subsequent requests while logged in are the unique cookie that
 //identifies the session. serialize user instances to and from the session
@@ -78,6 +56,30 @@ passport.deserializeUser(function(id, done) {
     done(err, user);
   });
 });
+
+app.use(express.static(__dirname + '/../react-client/dist'));
+
+
+//michael new
+//
+
+
+//handles a login attempt from the front end
+//returns 401 unauthorized if fails
+// app.post('/login',
+//   passport.authenticate('local'),
+//   function(req, res) {
+//     // If this function gets called, authentication was successful.
+//     // `req.user` contains the authenticated user.
+//     res.redirect('/users/' + req.user.username);
+//   });
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
+
 
 
 
