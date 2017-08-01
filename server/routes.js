3b00046510fa6ea945b6185f4796e3389b025842
Adrienne Tran <adrienne@adriennetran.com>
2017-08-01 16:28:04 -0400

    Refactored routes

--- server/routes.js
+++ server/routes.js
@@ -6,8 +6,10 @@ const router = express.Router();
 // Creating a new meeting/checking if meeting exists?
 router.post('/meetings', function (req, res) {
   console.log('meetings post');
+  var meeting = new Meeting({  });
+
+  res.statusCode(201);
   res.send();
-  // var meeting = new Meeting({  })
 });
 
 // Getting the results of the match
