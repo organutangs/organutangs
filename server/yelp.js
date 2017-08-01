25af048baed23e1d9bfea616bb02e9d765c84bbc
Nick <nicholaspowel@users.noreply.github.com>
2017-08-01 14:14:46 -0400

    modding yelp request

--- server/yelp.js
+++ server/yelp.js
@@ -1,11 +1,18 @@
-
 // TODO : Make a get request to the Yelp API and get a list of locations
 
 //takes an object
 function yelpRequest(location, term = food, dist = 1000) {
-  var long =;
-  var lat =;
+  var long = ;
+  var lat = ;
   var url = `https://api.yelp.com/v3/businesses/search&term=${term}&latitude=${lat}&longitude=${long}&radius=${dist}&limit=20`;
-  return url;
-};
 
+  axios.get(url)
+    .then((res) => {
+      // do something with the return data
+      console.log(coordinates[mid]);
+      return list;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
