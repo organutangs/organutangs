5855e99789a5f0c164494d655e09127780d703df
Nick <nicholaspowel@users.noreply.github.com>
2017-08-01 14:10:26 -0400

    Modified yelp request file

--- server/yelp.js
+++ server/yelp.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+
 // TODO : Make a get request to the Yelp API and get a list of locations
 
-=======
 //takes an object
 function yelpRequest(location, term = food, dist = 1000) {
   var long =;
@@ -9,4 +8,4 @@ function yelpRequest(location, term = food, dist = 1000) {
   var url = `https://api.yelp.com/v3/businesses/search&term=${term}&latitude=${lat}&longitude=${long}&radius=${dist}&limit=20`;
   return url;
 };
->>>>>>> 785cf74e0b7ad5168b238d4228b11d1743e29a6e
+
