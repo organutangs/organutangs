9ca97e83b956aa9d3dc443cb54524ea79064b1eb
Adrienne Tran <adrienne@adriennetran.com>
2017-07-31 20:50:15 -0400

    First iteration of generateMidpoint function

---
+++ server/google-maps.js
@@ -0,0 +1,14 @@
+// generateMidpoint
+// I: Two arrays with 2 elements, the first representing the latitude coordinate and the second element representing the longitude coordinate (e.g [37.4228642, -122.0851557])
+// O: An array with two elements which represents the midpoint coordinate
+
+var generateMidpoint = (coord1, coord2) => {
+  var avgLat= ( coord1[0] + coord2[0] ) / 2;
+  var avgLon = ( coord1[1] + coord2[1] ) / 2;
+
+  return [ avgLat, avgLon ];
+};
+
+var google = [37.4228642, -122.0851557];
+var home = [37.77594788029151, -122.3985733197085];
+console.log(generateMidpoint(google, home));
