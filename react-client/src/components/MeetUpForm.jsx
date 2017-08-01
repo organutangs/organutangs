92d3b7b14ef224cc623916f01f19fa02846ab0da
Adrienne Tran <adrienne@adriennetran.com>
2017-08-01 18:49:59 -0400

    Revised post request

--- react-client/src/components/MeetUpForm.jsx
+++ react-client/src/components/MeetUpForm.jsx
@@ -8,52 +8,54 @@ class MeetUpForm extends React.Component {
     this.state = {
       sessionUser: 'User1',
       friendId: '',
-      userLocation: ''
+      userLocationAddress: ''
     };
 
     this.handleFriendChange = this.handleFriendChange.bind(this);
     this.handleAddressChange = this.handleAddressChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.performPostRequest = this.performPostRequest.bind(this);
-  } 
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   handleFriendChange(event) {
-    this.setState({friendId: event.target.value});
+    this.setState({ friendId: event.target.value });
   }
 
   handleAddressChange(event) {
-    this.setState({userLocation: event.target.value});
+    this.setState({ userLocationAddress: event.target.value });
   }
 
   handleSubmit(e) {
-    console.log('handleSubmit');
     e.preventDefault();
-    this.performPostRequest();
-    alert('Retrieving Join information with friend ' + this.state.friendId
-           + ' starting from address, ' + this.state.userLocation);
+    // { friendId, userLocationAddress } = this.state;
+    var friendId = this.state.friendId;
+    var userLocationAddress = this.state.userLocationAddress;
+
+    axios.post('/meetings', {
+      friendId,
+      userLocation: {
+        "address" : userLocationAddress,
+        "coordinates": [0,0]
+      }
+    })
+      .then(function (response) {
+        console.log('Posted successfully');
+      })
+      .catch(function (error) {
+        alert('error ' + error);
+      });
   }
 
-  performPostRequest() {
-    console.log('PerformPostRequest');
-    axios.post('/meetings', this.state)
-  .then(function (response) {
-    alert('response ' + response);
-  })
-  .catch(function (error) {
-    alert('error ' + error);
-  });
-}
   render(){
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
           <label>
             USER TO MEET:
-            <input type="text" value={this.state.friendId} onChange ={this.handleFriendChange} />
+            <input type="text" value={ this.state.friendId } onChange ={this.handleFriendChange} />
           </label>
           <label>
             YOUR ADDRESS:
-            <input type="text" value={this.state.userLocation} onChange={this.handleAddressChange} />
+            <input type="text" value={ this.state.userLocationAddress } onChange={this.handleAddressChange} />
           </label>
           <input type="submit" value="JOIN" />
         </form>
