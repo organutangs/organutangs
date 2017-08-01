30d6357837d643dc0076df0938c48128e3931036
Adrienne Tran <adrienne@adriennetran.com>
2017-08-01 19:50:05 -0400

    Address coordinates are now pre-saved to db

--- database-mongo/models/meeting.js
+++ database-mongo/models/meeting.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-
+const config = require('../../server/config.js');
+var http = require('http');
 var schema = mongoose.Schema;
+var axios = require('axios');
 
 //this schema is used to set up a meeting
 var MeetingSchema = schema({
@@ -14,9 +16,19 @@ var MeetingSchema = schema({
 
 // pre save hook
 MeetingSchema.pre('save', function(next) {
-  console.log('this.userLocation', this.userLocation);
-  // var err = new Error('something went wrong');
-  next();
+  var APIKEY = config.google.APIKEY;
+  // Replaces spaces in path with %20
+  var address = encodeURIComponent((this.userLocation.address).trim());
+  var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${APIKEY}`
+
+  axios.get(geocodeUrl)
+    .then((res) => {
+      var lat = res.data.results[0].geometry.location.lat;
+      var lng = res.data.results[0].geometry.location.lng;
+      this.userLocation.coordinates = [ lat, lng ];
+      next();
+    })
+    .catch((err) => console.error(err));
 });
 
 var Meeting = mongoose.model('Meeting', MeetingSchema);
