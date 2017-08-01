4b17649cdef4005064cdea83fa446692f850f0b0
Michael Lam <mmmikeal@gmail.com>
2017-08-01 12:31:34 -0400

    Backend user auth, requires testing

--- server/index.js
+++ server/index.js
@@ -3,28 +3,55 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
 var morgan = require('morgan');
-
-//database
 var result = require('../database-mongo');
-
 //michael new
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./server/users');
 
 
 //middleware
 var app = express();
 app.use(passport.initialize());
 app.use(session({
-  secret:'secret',
+  secret: 'secret',
   saveUninitialized: true,
   resave: true
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('combined'));
-//am i missing router
+app.use('/users', users);
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.'), root = namespace.shift(), formParam = root;
+
+    while (namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
 
 //this configures the LocalStrategy for username/password authentication
@@ -87,7 +114,7 @@ app.post('/login',
 //getting the results of the match
 app.get('/results', function (req, res) {
   result.Match(function(err, data) {
-    if(err) {
+    if (err) {
       res.sendStatus(500);
     } else {
       res.json(data);
@@ -98,7 +125,7 @@ app.get('/results', function (req, res) {
 //getting the user info
 app.get('/users', function (req, res) {
   result.Match(function(err, data) {
-    if(err) {
+    if (err) {
       res.sendStatus(500);
     } else {
       res.json(data);
@@ -109,7 +136,7 @@ app.get('/users', function (req, res) {
 //creating a new meeting/checking if meeting exists?
 app.post('/meetings', function (req, res) {
   result.Match(function(err, data) {
-    if(err) {
+    if (err) {
       res.sendStatus(500);
     } else {
       res.json(data);
@@ -120,7 +147,7 @@ app.post('/meetings', function (req, res) {
 //create a new user
 app.post('/users', function (req, res) {
   result.Match(function(err, data) {
-    if(err) {
+    if (err) {
       res.sendStatus(500);
     } else {
       res.json(data);
