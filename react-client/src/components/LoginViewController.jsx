e28b693e6026846cc7ac4e35d41ad8809059829f
Michael Lam <mmmikeal@gmail.com>
2017-08-02 15:41:12 -0400

    Login Auth front end

--- react-client/src/components/LoginViewController.jsx
+++ react-client/src/components/LoginViewController.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import axios from 'axios';
+import MeetUpForm from './MeetUpForm.jsx';
+import Title from './Title.jsx';
+import Map from './Map.jsx';
+import Login from './Login.jsx';
+import Register from './Register.jsx';
 
-class LoginViewController extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
+function LoginViewController (props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) { //render our app if logged in
     return (
-
-    );
+      <div>
+        <Title />
+        <MeetUpForm />
+        <div style={{width:500, height:600, backgroundColor:'red', border: '2px solid black'}}>
+          <Map
+            containerElement={<div style={{height:100+'%'}} />}
+            mapElement={<div style={{height:100+'%'}} />}
+          />
+        </div>
+      </div>)
+  } else { //else render login
+    return (
+      <div>
+        <Login/>
+        <Register/>
+      </div>
+    )
   }
 }
 
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -5,43 +5,29 @@ import List from './components/List.jsx';
 import axios from 'axios';
 import MeetUpForm from './components/MeetUpForm.jsx';
 import Title from './components/Title.jsx';
+import Map from './components/Map.jsx';
+import LoginViewController from './components/LoginViewController.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
-    }
+      items: [],
+      auth: true
+    };
   }
 
   componentDidMount() {
-    $.ajax({
-      url: '',
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
+    //axios promise to backend for passport auth intialize auth state
   }
 
   render () {
-    
+
     return (
       <div>
-        <Title /> 
-        <MeetUpForm />
-        <div style={{width:500, height:600, backgroundColor:'red', border: '2px solid black'}}>
-          <Map 
-            containerElement={<div style={{height:100+'%'}} />}
-            mapElement={<div style={{height:100+'%'}} />} 
-          />
-        </div>  
-    </div>)
-  }   
+       <LoginViewController isLoggedIn={this.state.auth}/>
+      </div>)
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
