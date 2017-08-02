30f7076c31e0d359b59177fe324b70fcd8364428
Nick <nicholaspowel@users.noreply.github.com>
2017-08-02 12:33:13 -0400

    modified index.jsx to includ map.jsx

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -8,6 +8,7 @@ import Title from './components/Title.jsx';
 import Map from './components/Map.jsx';
 import LoginViewController from './components/LoginViewController.jsx';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
