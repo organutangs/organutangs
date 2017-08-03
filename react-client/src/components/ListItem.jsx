387dc64adf3f9dc65b206b2e9d073f0b192aadf8
Nick <nicholaspowel@users.noreply.github.com>
2017-08-02 23:16:36 -0400

    remove categories from yelp results

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const ListItem = (props) => (
   
   	<div className="yelp-list-entry-container">
+  	<div><pre>{console.log(props.item)}</pre></div>
 	    <div className="yelp-list-entry">
 	        <div className="media-left media-middle">
 	          <img className="listing-object" src={props.item.image_url} alt="" />
@@ -12,7 +13,6 @@ const ListItem = (props) => (
 	          <div className="yelp-list-entry-rating">{'Rating: ' + props.item.rating}</div>
 	        	<div className="yelp-list-entry-price">{'Price: ' + props.item.price}</div>
 	        	<div className="yelp-list-entry-reviews">{'Reviews: ' + props.item.review_count}</div>
-	          <div className="yelp-list-entry-categories">{'Categories: ' + props.item.categories[0].title}</div>
 	          <div className="yelp-list-entry-address">{'Address: ' + props.item.location.address1}</div>
 	        </div>
 	      </div>
