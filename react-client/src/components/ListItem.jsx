3ccb536e782a8bdfbd615e7578b25bcb8fd28284
Nick <nicholaspowel@users.noreply.github.com>
2017-08-02 23:00:50 -0400

    improved appearance of yelp list

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 
 const ListItem = (props) => (
-  <div>
-    <div className="yelp-list-entry">
-        <div className="media-left media-middle">
-          <img className="listing-object" src={props.item.image_url} alt="" />
-          </div>
-        <div className="listing-body">
-          <div className="yelp-list-entry-name" onClick={(e)=> props.handleClick(props.item)}>{props.item.name}</div>
-          <div className="yelp-list-entry-rating">{'Rating: ' + props.item.rating}</div>
-        	<div className="yelp-list-entry-price">{'Price: ' + props.item.price}</div>
-        	<div className="yelp-list-entry-reviews">{'Reviews: ' + props.item.review_count}</div>
-          <div className="yelp-list-entry-categories">{'Category: ' + props.item.categories.title}</div>
-          <div className="yelp-list-entry-address">{'Address: ' + props.item.location.address1}</div>
-        </div>
+  
+  	<div className="yelp-list-entry-container">
+	    <div className="yelp-list-entry">
+	        <div className="media-left media-middle">
+	          <img className="listing-object" src={props.item.image_url} alt="" />
+	         </div>
+	        <div className="listing-body">
+	          <div className="yelp-list-entry-name" onClick={(e)=> props.handleClick(props.item)}>{props.item.name}</div>
+	          <div className="yelp-list-entry-rating">{'Rating: ' + props.item.rating}</div>
+	        	<div className="yelp-list-entry-price">{'Price: ' + props.item.price}</div>
+	        	<div className="yelp-list-entry-reviews">{'Reviews: ' + props.item.review_count}</div>
+	          <div className="yelp-list-entry-categories">{'Categories: ' + props.item.categories[0].title}</div>
+	          <div className="yelp-list-entry-address">{'Address: ' + props.item.location.address1}</div>
+	        </div>
+	      </div>
       </div>
-  </div>
+  
 )
 
 export default ListItem;
