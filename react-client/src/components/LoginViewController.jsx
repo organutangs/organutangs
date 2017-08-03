bc9e2010dac8c424c36823bd2524d29eb7b5b0d2
Nick <nicholaspowel@users.noreply.github.com>
2017-08-03 14:25:26 -0400

    visual interface imporvements

---
+++ react-client/src/components/LoginViewController.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+import MeetUpForm from './MeetUpForm.jsx';
+import Title from './Title.jsx';
+import Map from './Map.jsx';
+import Login from './Login.jsx';
+import Register from './Register.jsx';
+import List from './List.jsx';
+
+function LoginViewController (props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) { //render our app if logged in
+    return (
+      <div>
+        <Title />
+        <MeetUpForm />
+        <div className="resultsContainer">
+          <div className= "mapBox" >
+            <div className="subMapBox">
+              <Map
+                containerElement={<div className="mapContainer" style={{height:100+'%'}} />}
+                mapElement={<div className="mapElement" style={{height:100+'%'}} />}
+              />
+            </div>
+          </div>
+          <div className="listContainer">
+            <List items={props.items}/>
+          </div>
+        </div>
+      </div>)
+  } else { //else render login
+    return (
+      <div>
+        <Login/>
+        <Register/>
+      </div>
+    )
+  }
+}
+
+export default LoginViewController;
