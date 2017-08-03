b0c230a877716b69176752c94f21effd078e32e6
Adrienne Tran <adrienne@adriennetran.com>
2017-08-02 23:03:09 -0400

    Merged with ed's front-end code that displays all places on the map

--- react-client/src/components/Map.jsx
+++ react-client/src/components/Map.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import {withGoogleMap, GoogleMap, Marker} from 'react-google-maps'
 class Map extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
 
   render(){
-    const markers = this.props.markers || [];
+    const markers = this.props.markers.map(function(obj,index){
+      return {
+        position:{
+          lat:obj.coordinates.latitude,
+          lng:obj.coordinates.longitude
+        }
+      }
+    });
+
     return(
       <GoogleMap
         defaultZoom={15}
-        defaultCenter={{ lat: 40.751094, lng: -73.987597 }}>
-        {markers.map((marker, index) => (
-          <Marker {...marker} />
-          )
+        defaultCenter={this.props.center}>
+        {markers.map((marker, index) => {
+            console.log('marker',marker);
+            return(
+              <Marker
+                key={index}
+                position={marker.position}
+              />
+            )
+          }
         )}
       </GoogleMap>
     )
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -6,15 +6,39 @@ import axios from 'axios';
 import Map from './components/Map.jsx';
 import MeetUpForm from './components/MeetUpForm.jsx';
 import Title from './components/Title.jsx';
-import Map from './components/Map.jsx';
 import LoginViewController from './components/LoginViewController.jsx';
+const io = require('socket.io-client');
+const socket = io();
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: sampleData,
-      auth: true
+      // items: sampleData,
+      auth: true,
+      meetingLocations: [{ id: 'tacos-y-quesadillas-mexico-manhattan',
+        name: 'Tacos Y Quesadillas Mexico',
+        image_url: 'https://s3-media4.fl.yelpcdn.com/bphoto/drLoFbH5OHpiptuadnXw_A/o.jpg',
+        is_closed: false,
+        url: 'https://www.yelp.com/biz/tacos-y-quesadillas-mexico-manhattan?adjust_creative=TuI7KPQ2iOcLaRBdE3LpMA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=TuI7KPQ2iOcLaRBdE3LpMA',
+        review_count: 37,
+        categories: [ [Object] ],
+        rating: 4.5,
+        coordinates: { latitude: 40.7687515765429, longitude: -73.9850422739983 },
+        transactions: [],
+        price: '$',
+        location:
+        { address1: '924 9th Ave',
+          address2: '',
+          address3: '',
+          city: 'Manhattan',
+          zip_code: '10019',
+          country: 'US',
+          state: 'NY',
+          display_address: [Array] },
+        phone: '',
+        display_phone: '',
+        distance: 309.03863120119996 }]
     };
   }
 
@@ -23,28 +47,30 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    socket.on('meeting locations', (data) => {
+      console.log('data', data);
+      this.setState({ meetingLocations: data });
+    })
   }
 
   render () {
 
     return (
       <div>
-<<<<<<< Updated upstream
-       <LoginViewController items={this.state.items} isLoggedIn={this.state.auth}/>
-      </div>)
-  }
-=======
+        {/*<LoginViewController items={this.state.items} isLoggedIn={this.state.auth}/>*/}
         <Title /> 
         <MeetUpForm />
         <div style={{width:500, height:600, backgroundColor:'red', border: '2px solid black'}}>
           <Map
+            markers={ this.state.meetingLocations }
+            center={{ lat: 40.751094, lng: -73.987597 }}
             containerElement={<div style={{height:100+'%'}} />}
             mapElement={<div style={{height:100+'%'}} />}
           />
-        </div>  
-    </div>)
-  }   
->>>>>>> Stashed changes
+        </div>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
