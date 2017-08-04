8bc03f1080727ad8b9a4a8fd24047c1504e16338
Nick <nicholaspowel@users.noreply.github.com>
2017-08-03 21:10:27 -0400

    remove console log

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -4,7 +4,6 @@ import ListItem from './ListItem.jsx';
 const List = (props) => (
   <div>
     <div className="yelp-list">
-      <pre>{ console.log('props', props) }</pre>
       {props.items.map((item, index) => <ListItem key={index} handleClick={props.handleClick} item={item}/>)}
     </div>
   </div>
